6cb414f99c8bf1e2b29af0efa51fe2068990b159
RobinLIn <robin60207@gmail.com>
2015-10-17 11:04:23 +0800

    count the execute time

--- q2/iterative.c
+++ q2/iterative.c
@@ -18,11 +18,29 @@ char smallest_character(char str[], char c)
     return str[0];
 }
 
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
 int main()
 {
+    struct timespec start, end;
+    double cpu_time;
     char a[] = {'c','f','j','p','v'};
+    clock_gettime(CLOCK_REALTIME, &start);
     char out = smallest_character(a,'a');
-    printf("%c\n", out);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time = diff_in_second(start, end);
 
+    printf("execution time of iterative : %lf sec\n", cpu_time);
     return 0;
 }
