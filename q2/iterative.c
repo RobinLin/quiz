1947b903078c980de715c29152441d35a9087a52
RobinLIn <robin60207@gmail.com>
2015-10-17 09:47:44 +0800

    implement Q2

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,28 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <assert.h>
+
+
+char smallest_character(char str[], char c)
+{
+    int i = 0;
+    while(i != '\0') {
+        if(str[i] > c) {
+            return str[i];
+        }
+        i++;
+    }
+    return str[0];
+}
 
 int main()
 {
+    char a[] = {'c','f','j','p','v'};
+    char out = smallest_character(a,'a');
+    printf("%c\n", out);
+
     return 0;
 }
