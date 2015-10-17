ed20ed08503e88f3d1d9d929757c300cff0d2bf8
RobinLIn <robin60207@gmail.com>
2015-10-17 22:17:23 +0800

    implement Q4

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,50 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+int maxSubArray(int A[], int n)
+{
+    int max_sum = 0, sum = 0;
+    for (int i=0; i<n; ++i) {
+        sum += A[i];            // 累計總和
+        if (sum < 0) sum = 0;   // 零總比負數好
+
+        // 隨時紀錄最大值
+        if (sum > max_sum) max_sum = sum;
+    }
+    return max_sum;
+
+}
 
 int main()
 {
+    struct timespec start, end;
+    double cpu_time;
+
+    int a[9] = { -2,1,-3,4,-1,2,1,-5,4 };
+    int ans;
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    ans = maxSubArray(a,9);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time = diff_in_second(start, end);
+
+    printf("ans = %d execution time = %lf nsec\n", ans, cpu_time);
+
     return 0;
 }
--- q4/recursive.c
+++ q4/recursive.c
@@ -1,6 +1,52 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+
+int maxSubArray(int A[], int n)
+{
+    int i = 0;
+    int maxsum = A[0];
+    int currentMax = A[0];
+
+    for (i = 1; i < n; i++) {
+        currentMax = max(A[i], currentMax + A[i]);
+        maxsum = max(maxsum, currentMax);
+    }
+    return maxsum;
+}
 
 int main()
 {
+    struct timespec start, end;
+    double cpu_time;
+
+    int a[9] = { -2,1,-3,4,-1,2,1,-5,4 };
+    int ans;
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    ans = maxSubArray(a,9);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time = diff_in_second(start, end);
+
+    printf("ans = %d execution time = %lf nsec\n", ans, cpu_time);
+
     return 0;
 }
